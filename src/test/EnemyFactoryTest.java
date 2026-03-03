db0428b97979cf52623856650bc8e3fc0909f842
Rhea Mimi Carillo <rheamimicarillo@gmail.com>
2026-03-03 06:37:45 -0800

    added JUnit 5 skeletons

---
+++ src/test/EnemyFactoryTest.java
@@ -0,0 +1,29 @@
+/**
+ * Test skeleton for EnemyFactory behavior.
+ *
+ * These are placeholders for future unit tests.
+ */
+import org.junit.jupiter.api.Test;
+
+public class EnemyFactoryTest {
+
+    @Test
+    void random_returnsEnemyWithValidCoreStats() {
+        // TODO Arrange / Act / Assert
+    }
+
+    @Test
+    void spawn_knownName_returnsMatchingEnemyType() {
+        // TODO Arrange / Act / Assert
+    }
+
+    @Test
+    void spawn_unknownName_throwsIllegalArgumentException() {
+        // TODO Arrange / Act / Assert
+    }
+
+    @Test
+    void xpAndDropLookups_returnFallbackForUnknownEnemy() {
+        // TODO Arrange / Act / Assert
+    }
+}
---
+++ src/test/InventoryTest.java
@@ -0,0 +1,29 @@
+/**
+ * Test skeleton for Inventory behavior.
+ *
+ * These are placeholders for future unit tests.
+ */
+import org.junit.jupiter.api.Test;
+
+public class InventoryTest {
+
+    @Test
+    void addWeapon_respectsCapacityRules() {
+        // TODO Arrange / Act / Assert
+    }
+
+    @Test
+    void equipWeapon_switchesActiveIndex() {
+        // TODO Arrange / Act / Assert
+    }
+
+    @Test
+    void addPotion_increasesStoredPotions() {
+        // TODO Arrange / Act / Assert
+    }
+
+    @Test
+    void ammoOperations_neverGoNegative() {
+        // TODO Arrange / Act / Assert
+    }
+}
